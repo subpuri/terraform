948a03ff417fba236f32466b979c49b90544cf9a
subpuri <subhpuri0@gmail.com>
2025-12-16 13:44:21 +0530

    removing cloud 9

--- main.tf
+++ main.tf
@@ -8,3 +8,12 @@ terraform {
     } 
   } 
 }
+
+resource "aws_instance" "example" {
+  ami           = "ami-0f5ee92e2d63afc18" # ap-south-1 Amazon Linux 2
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraform-test"
+  }
+}
